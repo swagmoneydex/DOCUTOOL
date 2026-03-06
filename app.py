eb25660b10659ba383b0ae65af17b8939ff79cf8
Dexter Stewart <dstewart@sfl.local>
2026-03-05 23:35:53 -0500

    Create tables on Render startup

--- app.py
+++ app.py
@@ -233,7 +233,9 @@ def compress():
     return '<h2>Compression tool coming next</h2>'
 
 
+with app.app_context():
+    db.create_all()
+
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
